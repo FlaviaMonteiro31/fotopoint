3571946e61f53fec774ae27a44b0ce9bbaeea66a
Flavia Monteiro <flavia_alvesmonteiro@yahoo.com.br>
2025-02-08 16:04:46 -0300

    Criando_estrutura

---
+++ fotopointapp/src/main/java/com/fotopoint/fotopointapp/FotopointappApplication.java
@@ -0,0 +1,13 @@
+package com.fotopoint.fotopointapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotopointappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotopointappApplication.class, args);
+	}
+
+}
---
+++ fotopointapp/src/test/java/com/fotopoint/fotopointapp/FotopointappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fotopoint.fotopointapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotopointappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
